a5036a9821e3dae013c341ffad15bb733b50c292
Joshua Schappel <joshuaschappel@Joshuas-MacBook-Pro.local>
2021-11-28 15:12:54 -0500

    Added zsh files

--- bash/install.sh
+++ bash/install.sh
@@ -4,11 +4,11 @@
 ln -svf $(pwd)/.bashrc $HOME
 ln -svf $(pwd)/.bash_aliases $HOME
 ln -svf $(pwd)/.bash_code $HOME
-ln -svf $(pwd)/init.vim $HOME/.nvimrc
-ln -svf $(pwd)/ginit.vim $HOME/.gnvim.rc
-ln -svf $(pwd)/gitconfig $HOME/.gitconfig
+ln -svf $(pwd)../git/.gitconfig $HOME
+ln -svf $(pwd)/../vim/init.vim $HOME/.nvimrc
+ln -svf $(pwd)/../vim/ginit.vim $HOME/.gnvim.rc
 
 
 # Softlink nvim files
-ln -svf $(pwd)/init.vim $HOME/.config/nvim
-ln -svf $(pwd)/ginit.vim $HOME/.config/nvim
+ln -svf $(pwd)/../vim/init.vim $HOME/.config/nvim
+ln -svf $(pwd)/../vim/ginit.vim $HOME/.config/nvim
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,1 +1,13 @@
 #!/bin/zsh
+
+# Softlink dotfiles to home area
+ln -svf $(pwd)/.zshrc $HOME
+ln -svf $(pwd)/.zsh_aliases $HOME
+ln -svf $(pwd)../git/.gitconfig $HOME
+ln -svf $(pwd)/../vim/init.vim $HOME/.nvimrc
+ln -svf $(pwd)/../vim/ginit.vim $HOME/.gnvim.rc
+#
+#
+## Softlink nvim files
+ln -svf $(pwd)/../vim/init.vim $HOME/.config/nvim
+ln -svf $(pwd)/../vim/ginit.vim $HOME/.config/nvim
