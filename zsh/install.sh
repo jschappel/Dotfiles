f83fcb0bb4ca3cb1b5221f0972feece38cc63fdd
jschappel <jmschappel12@gmail.com>
2023-08-14 08:32:27 -0400

    Updated Dotfiles

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,6 +6,10 @@ ln -sfv $HEREP/.zprofile $HOME
 ln -sfv $HEREP/.oh_my_zsh_config $HOME
 ln -sfv $HERE/bash/.bash_aliases $HOME/.zsh_aliases
 
+if [ -d ~/.oh-my-zsh ]; then 
+  ln -sfv $HEREP/my_theme.zsh-theme $ZSH/custom/themes
+fi
+
 if [ "$LINK_WORK" == "true" ]; then
   echo "Nothing to link for work"
 fi
